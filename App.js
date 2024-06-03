e6a30be265de9d924a1be7753c88794ed7bda7f7
oguzhanuyanik-sr <oguzhanuyanik.sr@gmail.com>
2024-06-03 15:08:47 +0300

    all validations done

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import {
+  Alert,
   StyleSheet,
   Text,
   TextInput,
@@ -9,43 +10,145 @@ import {
 import { Formik } from 'formik';
 import * as Yup from 'yup';
 
+const SignupSchema = Yup.object().shape({
+  name: Yup.string()
+    .min(6, 'Too Short!')
+    .max(50, 'Too Long!')
+    .required('Required'),
+  email: Yup.string().email('Invalid email').required('Required'),
+  password: Yup.string()
+    .min(8, 'Password must be at least 8 characters')
+    .required('Required')
+    .matches(
+      /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/,
+      'Incorrect Format'
+    ),
+  confirmPassword: Yup.string()
+    .min(8, 'Too Short!')
+    .oneOf([Yup.ref('password')], 'Passwords do not match.')
+    .required('Required'),
+  mobile: Yup.string()
+    .min(10, 'Must be exactly 10 degits')
+    .max(10, 'Must be exactly 10 degits')
+    .matches(/^[0-9]+$/, 'Must be only digits')
+    .required('Required'),
+});
+
 export default function App() {
   return (
-    <Formik>
-      <View style={styles.wrapper}>
-        <StatusBar style='light' />
+    <Formik
+      initialValues={{
+        name: '',
+        email: '',
+        password: '',
+        confirmPassword: '',
+        mobile: '',
+      }}
+      validationSchema={SignupSchema}
+      onSubmit={(values) => Alert.alert(JSON.stringify(values))}
+    >
+      {({
+        values,
+        errors,
+        touched,
+        handleChange,
+        setFieldTouched,
+        isValid,
+        handleSubmit,
+      }) => (
+        <View style={styles.wrapper}>
+          <StatusBar style='light' />
 
-        <View style={styles.formContainer}>
-          <Text style={styles.title}>Sign Up</Text>
+          <View style={styles.formContainer}>
+            <Text style={styles.title}>Sign Up</Text>
 
-          <View style={styles.inputWrapper}>
-            <TextInput style={styles.inputStyle} placeholder='Full Name' />
+            <View style={styles.inputWrapper}>
+              <TextInput
+                style={styles.inputStyle}
+                placeholder='Full Name'
+                value={values.name}
+                onChangeText={handleChange('name')}
+                onBlur={() => setFieldTouched('name')}
+              />
+              {touched.name && errors.name && (
+                <Text style={styles.errorTxt}>{errors.name}</Text>
+              )}
+            </View>
+
+            <View style={styles.inputWrapper}>
+              <TextInput
+                style={styles.inputStyle}
+                placeholder='Email Address'
+                autoCapitalize='none'
+                value={values.email}
+                onChangeText={handleChange('email')}
+                onBlur={() => setFieldTouched('email')}
+              />
+              {touched.email && errors.email && (
+                <Text style={styles.errorTxt}>{errors.email}</Text>
+              )}
+            </View>
+
+            <View style={styles.inputWrapper}>
+              <TextInput
+                style={styles.inputStyle}
+                autoCapitalize='none'
+                placeholder='Password'
+                value={values.password}
+                onChangeText={handleChange('password')}
+                onBlur={() => setFieldTouched('password')}
+                secureTextEntry={true}
+              />
+
+              {touched.password && errors.password && (
+                <Text style={styles.errorTxt}>{errors.password}</Text>
+              )}
+            </View>
+
+            <View style={styles.inputWrapper}>
+              <TextInput
+                style={styles.inputStyle}
+                placeholder='Confirm Password'
+                secureTextEntry={true}
+                autoCapitalize='none'
+                value={values.confirmPassword}
+                onChangeText={handleChange('confirmPassword')}
+                onBlur={() => setFieldTouched('confirmPassword')}
+              />
+              {touched.confirmPassword && errors.confirmPassword && (
+                <Text style={styles.errorTxt}>{errors.confirmPassword}</Text>
+              )}
+            </View>
+
+            <View style={styles.inputWrapper}>
+              <TextInput
+                style={styles.inputStyle}
+                placeholder='Mobile Number'
+                value={values.mobile}
+                onChangeText={handleChange('mobile')}
+                onBlur={() => setFieldTouched('mobile')}
+                keyboardType='phone-pad'
+              />
+              {touched.mobile && errors.mobile && (
+                <Text style={styles.errorTxt}>{errors.mobile}</Text>
+              )}
+            </View>
+
+            <TouchableOpacity
+              onPress={handleSubmit}
+              style={[
+                styles.submitBtn,
+                {
+                  backgroundColor: isValid ? 'green' : 'grey',
+                },
+              ]}
+              disabled={!isValid}
+            >
+              <Text style={styles.submitBtnTxt}>Submit</Text>
+            </TouchableOpacity>
           </View>
-
-          <View style={styles.inputWrapper}>
-            <TextInput style={styles.inputStyle} placeholder='Email Address' />
-          </View>
-
-          <View style={styles.inputWrapper}>
-            <TextInput style={styles.inputStyle} placeholder='Password' />
-          </View>
-
-          <View style={styles.inputWrapper}>
-            <TextInput
-              style={styles.inputStyle}
-              placeholder='Confirm Password'
-            />
-          </View>
-
-          <View style={styles.inputWrapper}>
-            <TextInput style={styles.inputStyle} placeholder='Mobile Number' />
-          </View>
-
-          <TouchableOpacity onPress={() => {}} style={styles.submitBtn}>
-            <Text style={styles.submitBtnTxt}>Submit</Text>
-          </TouchableOpacity>
         </View>
-      </View>
+      )}
     </Formik>
   );
 }
@@ -84,7 +187,6 @@ const styles = StyleSheet.create({
     color: '#ff0d10',
   },
   submitBtn: {
-    backgroundColor: '#395b64',
     padding: 10,
     borderRadius: 15,
     justifyContent: 'center',
